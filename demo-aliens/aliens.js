8d377e88376bbb61f4e961a6b809c6e3c6ab6968
Dominic Tremblay <dominictremblay24@gmail.com>
2021-11-16 07:09:36 -0500

    demo-reset

--- demo-aliens/aliens.js
+++ demo-aliens/aliens.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
-  $('.alien-btn').on('click', function(event) {
-    const $parentSection = $(event.target).closest('section');
-    const $talkP = $parentSection.find('.talk');
-
-    $talkP.append('💬');
-  });
-
+ 
 }); // document ready
