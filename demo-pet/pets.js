16a77f746c438ffe0391b730964d56c423174254
Dominic Tremblay <dominictremblay24@gmail.com>
2021-12-14 12:53:29 -0500

    final demo

--- demo-pet/pets.js
+++ demo-pet/pets.js
@@ -21,3 +21,63 @@ const pets = [
     age: 2,
   },
 ];
+
+const createPetElement = function (petObj) {
+
+
+
+  // create the HTML
+  const petElement = `<article>
+  <header>
+    <h2>${petObj.name}</h2>
+  </header>
+  <div class="content">
+    <ul>
+      <li><span>Species: </span>${petObj.type}</li>
+      <li><span>Age: </span>${petObj.age}</li>
+    </ul>
+  </div>
+  <footer>${petObj.name} is a >${petObj.type} and is ${petObj.age} years old</footer>
+</article>`;
+
+  return petElement;
+};
+
+// loop through the petObjects
+// call createElement
+// append it to the section
+const renderPetElements = function (target) {
+  // loop through the list of pets
+
+  for (let petObj of pets) {
+    // Create the pet HTML element
+    const newElement = createPetElement(petObj);
+    // attach the new pet HTML element to the DOM => append
+    target.append(newElement);
+
+  }
+};
+
+$(document).ready(function () {
+
+
+  // const petContainer = document.querySelector('#pets-container');
+  const petContainer = $('#pets-container');
+
+  renderPetElements(petContainer);
+
+  const frm = $('#frm-pet');
+
+  frm.on('submit', function(event){
+
+    event.preventDefault();
+
+    // extract the info from the form
+
+    // create the petElement
+
+    // add it to its container
+
+  })
+  
+});
--- demo-todo/app.js
+++ demo-todo/app.js
@@ -5,3 +5,52 @@
 // Manipulating documents Documentation
 // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents
 // https://developer.mozilla.org/en-US/docs/Web/API/Document
+
+// When? There is a click on the submit button
+// add event listener => submit event
+
+const frm = document.querySelector('#new-todo-frm');
+
+frm.addEventListener('submit', function(event) {
+
+  // prevent the default form submission by the browser => no page relaod
+  event.preventDefault();
+
+  // refer to the current element that the event occured on
+  // using event.target
+  console.log("target:", event.target);
+  // or this
+  console.log("this", this);
+// target the input box
+  const inputBox = this.elements.todo;
+// to extract the todo content
+  const todoContent = inputBox.value;
+
+  console.log(todoContent);
+// create the new element (li)
+  const newLi = document.createElement('li');
+
+
+// change its content
+  newLi.innerText = todoContent;
+
+  console.log(newLi);
+
+// Add it the the list (ul)
+// target the parent element => append
+  const list = document.querySelector('#todos');
+
+  list.append(newLi);
+
+  // Reset our input box
+  inputBox.value = '';
+  inputBox.focus();
+
+
+});
+
+
+
+
+
+
